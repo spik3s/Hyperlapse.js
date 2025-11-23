00ddcb3f19d57d02b2936aebb47ac4095671ebc3
Jacek 'Spikes' Wozniak <github@spik3s.com>
2025-11-23 15:33:43 +0700

    First version of new UI

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        index: resolve(__dirname, 'index.html'),
         simple: resolve(__dirname, 'examples/simple.html'),
         viewer: resolve(__dirname, 'examples/viewer.html'),
       },
